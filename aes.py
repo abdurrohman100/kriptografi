872ad8ef118fcb8ec0f97ab49ed4aa72f356a130
Abdur Rohman <abdrohman0100@gmail.com>
2022-03-31 13:57:30 +0000

    patch

---
+++ aes.py
@@ -0,0 +1,119 @@
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+from aes_scratch import CBC
+from base64 import b64encode, b64decode
+
+class AESCipher(object):
+    def __init__(self, key, scratch = False):
+        self.block_size = AES.block_size
+        self.scratch = scratch
+        if(scratch):
+            self.key = hashlib.md5(key.encode()).digest()
+        else :
+            self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, plain_text):
+        plain_text = self.__pad(plain_text)
+        iv = Random.new().read(self.block_size)
+        if self.scratch :
+            cipher = CBC(self.key, plain_text.encode(), iv)
+            encrypted_text = cipher.encrypt()
+        else :
+            cipher = AES.new(self.key, AES.MODE_CBC, iv)
+            encrypted_text = cipher.encrypt(plain_text.encode())
+        return b64encode(iv + encrypted_text).decode("utf-8")
+
+    def encrypt_byte(self, byte):
+        byte = self.__pad_byte(byte)
+        iv = Random.new().read(self.block_size)
+        if self.scratch :
+            cipher = CBC(self.key, byte, iv)
+            encrypted_byte = cipher.encrypt()
+        else :
+            cipher = AES.new(self.key, AES.MODE_CBC, iv)
+            encrypted_byte = cipher.encrypt(byte)
+        return iv + encrypted_byte
+
+    def decrypt(self, encrypted_text):
+        encrypted_text = b64decode(encrypted_text)
+        iv = encrypted_text[:self.block_size]
+        if self.scratch :
+            cipher = CBC(self.key, encrypted_text[self.block_size:], iv)
+            try:
+                plain_text = cipher.decrypt().decode("utf-8")
+                return self.__unpad(plain_text)
+            except:
+                plain_text = " Cannot decrypt"
+        else :
+            cipher = AES.new(self.key, AES.MODE_CBC, iv)
+            try:
+                plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
+                return self.__unpad(plain_text)
+            except:
+                plain_text = " Cannot decrypt"
+        return plain_text
+
+    def decrypt_byte(self, encrypted_byte):
+        iv = encrypted_byte[:self.block_size]
+        if self.scratch :
+            cipher = CBC(self.key, encrypted_byte[self.block_size:], iv)
+            byte = cipher.decrypt()
+        else :
+            cipher = AES.new(self.key, AES.MODE_CBC, iv)
+            byte = cipher.decrypt(encrypted_byte[self.block_size:])
+        return self.__unpad(byte)
+
+    def __pad(self, plain_text):
+        number_of_bytes_to_pad = self.block_size - len(plain_text) % self.block_size
+        ascii_string = chr(number_of_bytes_to_pad)
+        padding_str = number_of_bytes_to_pad * ascii_string
+        padded_plain_text = plain_text + padding_str
+        return padded_plain_text
+
+    def __pad_byte(self, byte):
+        byte_array = bytearray(byte)
+        number_of_bytes_to_pad = self.block_size - len(byte_array) % self.block_size
+        for i in range(0, number_of_bytes_to_pad) :
+            byte_array.append(number_of_bytes_to_pad)
+        return bytes(byte_array)
+
+    @staticmethod
+    def __unpad(plain_text):
+        last_character = plain_text[len(plain_text) - 1:]
+        return plain_text[:-ord(last_character)]
+
+    @staticmethod
+    def __unpad_byte(byte):
+        last_byte = byte[len(byte) - 1:]
+        return byte[:-ord(last_byte)]
+
+from time import time
+if __name__=="__main__":
+    aes = AESCipher("abcdjklauiofjdjd", True)
+    t0 = time()
+    enc = aes.encrypt_byte("aaaaaaaaaaaaaaaxybbbbbbbbbbbbbbb".encode())
+    t1 = time()
+    # print(b64encode(enc).decode())
+    dec = aes.decrypt_byte(enc)
+    t2 = time()
+
+    print('message')
+    message = 'aaaaaaaaaaaaaaaxybbbbbbbbbbbbbbb'
+    print(message)
+    print()
+
+    print('encryption took ' + str(t1-t0)+ ' seconds')
+    enc = aes.encrypt("aaaaaaaaaaaaaaaxybbbbbbbbbbbbbbb")
+    print(enc)
+    print()
+
+    print('decryption took ' + str(t2-t1) + ' seconds')
+    print(aes.decrypt(enc))
+    print()
+
+    # print(dec)
+    # print(aes.encrypt("a"))
+    # print(aes.decrypt(aes.encrypt("a")))
+
+
---
+++ client.py
@@ -0,0 +1,151 @@
+import base64
+import logging
+import socket
+import os
+import json
+from DiffieHellman import DiffieHellman
+# from aes import AESCipher
+
+AESC = AESCipher("kijpakbas", True)
+
+diffieHelman = DiffieHellman()
+
+# AESDict = {}
+
+TARGET_IP = "127.0.0.1"
+TARGET_PORT = 8889
+
+cipher = AESCipher("ini key", True)
+
+class ChatClient:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = (TARGET_IP,TARGET_PORT)
+        self.sock.connect(self.server_address)
+        self.username=""
+        self.tokenid=""
+    def proses(self,cmdline):
+        j=cmdline.split(" ")
+        try:
+            command=j[0].strip()
+            if (command=='auth'):
+                username=j[1].strip()
+                password=j[2].strip()
+                response = self.login(username,password)
+                return response
+            elif (command=='send_file'):
+                usernameto = j[1].strip()
+                filename = j[2].strip()
+                return self.sendfile(usernameto,filename)
+                return self.sendgroupfile(groupto,filename)
+            elif (command=='my_file'):
+                return self.myfile()
+            elif (command=='download_file'):
+                username = j[1].strip()
+                filename = j[2].strip()
+                return self.downloadfile(username, filename)
+            elif (command=='sendkey'):
+                key = j[1].strip()
+                return self.sendkey(key)
+            elif (command=='getkey'):
+                username = j[1].strip()
+                return self.getkey(username)
+            else:
+                return "*Maaf, command tidak benar"
+        except IndexError:
+                return "-Maaf, command tidak benar"
+    def sendstring(self,string):
+        try:
+            self.sock.sendall(string.encode())
+            receivemsg = ""
+            while True:
+                data = self.sock.recv(64)
+                # print("diterima dari server",data)
+                if (data):
+                    receivemsg = "{}{}" . format(receivemsg,data.decode())  #data harus didecode agar dapat di operasikan dalam bentuk string
+                    if receivemsg[-4:]=='\r\n\r\n':
+                        # print("end of string")
+                        return json.loads(receivemsg)
+        except:
+            self.sock.close()
+            return { 'status' : 'ERROR', 'message' : 'Gagal'}
+
+    def login(self,username,password):
+        string="auth {} {} \r\n" . format(username,password)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            self.tokenid=result['tokenid']
+            self.username = username
+            self.proses(f"sendkey {diffieHelman.publicKey}")
+            return { 'status' : 'OK', 'message' : 'Logged In', 'username':username, 'token':self.tokenid}
+        else:
+            return { 'status' : 'ERROR', 'message' : 'Wrong Password or Username'}
+
+    def sendfile(self, usernameto, filename):
+        if(self.tokenid==""):
+            return "Error, not authorized"
+        try :
+            file = open(filename, "rb")
+        except FileNotFoundError :
+            return "Error, {} file not found".format(filename)
+        buffer = file.read()
+        encrypted_buffer = cipher.encrypt_byte(buffer)
+        encrypted_file = open(filename+".enc", "wb")
+        encrypted_file.write(encrypted_buffer)
+        file.close()
+        encrypted_file.close()
+        buffer_string = base64.b64encode(encrypted_buffer).decode('utf-8')
+        message="send_file {} {} {} {} \r\n" .format(self.tokenid, usernameto, filename, buffer_string)
+        result = self.sendstring(message)
+        if result['status']=='OK':
+            return {'status' : 'OK', 'message':'file sent to {}' . format(usernameto)}
+        else:
+            return {'status':'ERROR', 'message':'Error, {}' . format(result['message'])}
+    def myfile(self):
+        if (self.tokenid==""):
+            return "Error, not authorized"
+        string="my_file {} \r\n" . format(self.tokenid)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            return "{}" . format(json.dumps(result['messages']))
+        else:   
+            return {'status':'ERROR', 'message':'Error, {}' . format(result['message'])}
+    def downloadfile(self, username, filename):
+        if (self.tokenid==""):
+            return "Error, not authorized"
+        string="download_file {} {} {} \r\n" . format(self.tokenid, username, filename)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            output_file = open(result['filename'], 'wb')
+            decrypted_buffer = cipher.decrypt_byte(base64.b64decode(result['data']))
+            output_file.write(decrypted_buffer)
+            output_file.close()
+            return {'status' : 'OK', 'message':'file {} downloaded' . format(filename)}
+        else:
+            return {'status':'ERROR', 'message':'Error, {}' . format(result['message'])}
+
+    def sendkey(self, key):
+        if (self.tokenid==""):
+            return "Error, not authorized"
+        string="sendkey {} {} \r\n" . format(self.tokenid,key)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            return "{}" . format(json.dumps(result['messages']))
+        else:
+            return {'status':'ERROR', 'message':'Error, {}' . format(result['message'])}
+    def getkey(self, username):
+        if (self.tokenid==""):
+            return "Error, not authorized"
+        string="getkey {} {} \r\n" . format(self.tokenid,username)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            return {'status':'OK', 'key':result['key']}
+        else:
+            return {'status':'ERROR', 'message':'Error, {}' . format(result['message'])}
+
+if __name__=="__main__":
+    cc = ChatClient()
+    while True:
+        cmdline = input("Command {}:" . format(cc.tokenid))
+        print(cc.proses(cmdline))
+
